b7723d1e84256aec10bc22c04c3f81fbb510d7eb
Vincent Castellano <surye80@gmail.com>
2016-06-26 19:22:40 -0700

    Android build updates.

--- TBNMobile/TBNMobile.Droid/MainActivity.cs
+++ TBNMobile/TBNMobile.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TBNMobile.Droid
 {
-	[Activity (Label = "TBNMobile", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "TBNMobile", Icon = "@drawable/hopgrenade", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
